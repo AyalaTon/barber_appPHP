53b732ff621fde2fd4819c832facbaf9fd2ccae1
federzvz <85709865+federzvz@users.noreply.github.com>
2022-05-26 23:27:07 -0300

    Listar las publicaciones realizado.

--- templates/Pages/publicaciones.php
+++ templates/Pages/publicaciones.php
@@ -1,21 +1,33 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Publicacion[]|\Cake\Collection\CollectionInterface $publicacion
  */
 ?>
+
+
+<link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <div class="barbero form">
     <?= $this->Flash->render() ?>
     <h3>Publicaciones</h3>
 </div>
+<?= $this->Html->css(['publicaciones']) ?>
 <div class="publicacion index content">
-    <?= $this->Html->link(__('New Publicacion'), array('controller' => 'Publicacion', 'action' => 'add'), ['class' => 'button float-right boton_cerrar'])?>
-    <h3><?= __('Publicacion') ?></h3>
-    <div class="table-responsive">
-        <table>
-        </table>
-    </div>
-    <div class="paginator">
-        
-    </div>
+    <?php foreach ($publicacionesInvertidas as $publicacion) : ?>
+        <div class="tweet-wrap">
+            <div class="tweet-header">
+                <img src="https://i.imgur.com/NtMjMeh.jpg" alt="" class="avator">
+                <div class="tweet-header-info">
+                    <?= $publicacion->barbershopInfo->nombre ?> <span>@BarberoDueño</span><span>01/18/2001 (En un futuro...)
+                    </span>
+                    <p><?= $publicacion->contenido ?></p>
+                </div>
+            </div>
+            <div class="tweet-img-wrap">
+                <img src="<?= $publicacion->imagen ?>" alt="" class="tweet-img">
+            </div>
+        </div>
+    <?php endforeach; ?>
 </div>
